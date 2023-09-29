3c5429c744fa3895fd847ff444109639d08f8005
coquevolqueta <alecortezp1996@gmail.com>
2023-09-29 19:06:10 -0400

    hecho en chasqui

--- js/poke.js
+++ js/poke.js
@@ -1,6 +1,22 @@
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+if(input1.checked)
+        Hipodoge
+    else if(Input2.checked)
+        Capipepo
+    else if(Input3.checked)
+        Ratigueya
+    else if(Input4.checked)
+        Langostelvis
+    else if(Input5.checked)
+        Tucapalma
+    else if(Input6.checked)
+        Pydos
 function seleccionarMascotaJugador(){
     alert('SELECCIONASTE TU MASCOTA')
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+window.addEventListener('load', iniciarJuego)
